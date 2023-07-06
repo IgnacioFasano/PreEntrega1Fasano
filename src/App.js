db8aa691d1b85839f738b0fb3a71df084bf77e22
Ignacio Fasano <nacho.fasano@gmail.com>
2023-07-06 07:32:41 -0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 
 export default  function App() {
   return (
-    <div className="App">
+    <BrowserRouter>
       <NavBar/>
-      <ItemListContainer greeting={'Bienvenidos a mi ecommerce'}/>
-    </div>
+      <Routes>
+      <Route path="/" element={<ItemListContainer greeting={'Tu nuevo celular te espera aquí'}/>} />
+      <Route path="/category/:id" element={<ItemListContainer greeting={'Tu nuevo celular te espera aquí'}/>} />
+      </Routes>
+      
+    </BrowserRouter>
   );
 }
--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,8 +1,30 @@
+import { useState, useEffect } from 'react';
+
+import data from "../data/products.json";
+
 import Container from 'react-bootstrap/Container';
 const ItemListContainer = ({ greeting }) => {
+    const [products, setProducts] = useState([])
+
+    useEffect(( )=> {
+        const promesa = new Promise((resolve, reject) => {
+            setTimeout(()=>{
+                resolve(data)
+            }, 2000)
+        })
+        promesa.then(result => setProducts(result))
+    }, [])
+
     return (
         <Container>
             <h1>{greeting}</h1>
+            {products.lenght === 0 ? (
+            <div><p>Loading...</p></div>
+            ): (
+                products.map( phone =>(
+                <div key={phone.id}>{phone.phone_model}</div>
+                ))
+            )}
         </Container>
     )
 }
